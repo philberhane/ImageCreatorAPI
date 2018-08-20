d2b3664722b5cf1667690796bc7b8237225cc8fc
Philemon Berhane <philberhane@yahoo.com>
2018-08-20 01:49:26 -0700

    slight changes

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ var images = require('./routes/images');
 var app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
