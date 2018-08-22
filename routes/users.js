5ebdfef2c3662a34491ddd0afc6574dc72281f8c
Philemon Berhane <philberhane@yahoo.com>
2018-08-22 13:20:03 -0700

    slight changes

--- routes/users.js
+++ routes/users.js
@@ -212,7 +212,7 @@ imgConvert.fromURL({
     url: source,
     quality: 100,//default 100
     output_format:"jpg",//default jpg
-    size: 300//default original
+    size: 'original'//default original
 },function(err,buffer,file){
     if(!err)
     {
