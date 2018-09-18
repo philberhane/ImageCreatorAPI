e82d7334ad68cf44089e02ccf00d24b246b874f7
Philemon Berhane <philberhane@yahoo.com>
2018-09-17 23:41:39 -0700

    Test

--- routes/users.js
+++ routes/users.js
@@ -235,7 +235,7 @@ router.post('/register', function (req, res) {
 });
 			
 
-passport.use('local', new LocalStrategy({
+passport.use(new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
         usernameField : 'email',
         passwordField : 'password',
