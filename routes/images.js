c57de2a4c03a1bba5c423bdde2b016bf68447564
Philemon Berhane <philberhane@yahoo.com>
2018-10-21 15:47:14 -0700

    server changes

--- routes/images.js
+++ routes/images.js
@@ -35,13 +35,13 @@ var client = wordpress.createClient({
 // "Aurora Borealis" by Frederic Edwin Church
 // Licensed under Public Domain via Wikimedia Commons
 //console.log(desfile)
-var filename = desfile.name;
-var file = fs.readFile( filename );
+//var filename = desfile.name;
+//var file = fs.readFileSync( filename );
 client.uploadFile(
 	{
-	name: filename,
+	name: desfile.name,
 	type: "image/jpg",
-	bits: file
+	bits: desfile.buffer
 }, function( error, data ) {
 	console.log( arguments );
 });
